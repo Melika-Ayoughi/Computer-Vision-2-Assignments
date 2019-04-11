ba9b87b2c9a3bdd210424f30281031c0f5a445cb
Stijn Verdenius <stijnverdenius@pop-os.localdomain>
2019-04-11 15:46:39 +0200

    changes

--- Assignment1/icp/icp_iteration.m
+++ Assignment1/icp/icp_iteration.m
@@ -5,13 +5,13 @@ function [R, t] = icp_iteration(A_1, A_2, R, t)
 centroid_1 = centroid(A_1);
 centroid_2 = centroid(A_2);
 
-centered_vectors_1 = A_1 - centroid_1;
-centered_vectors_2 = A_2 - centroid_2;
+centered_vectors_1 = A_1 - t;
+centered_vectors_2 = A_2 - t;
 
 covariance = cov(centered_vectors_1, centered_vectors_2);
 
-R = svd(covariance);
-
 t = centroid_2 - R * centroid_1;
+
+R = svd(covariance);
 
 end
--- Assignment1/icp/rms.m
+++ Assignment1/icp/rms.m
@@ -4,6 +4,4 @@ function error = rms(A_1, A_2, psi, R, t)
 % TO DO: implement
 error = 0; 
 
-
-
 end

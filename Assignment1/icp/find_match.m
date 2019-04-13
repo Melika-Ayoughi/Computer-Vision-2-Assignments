44f630fa5d7958ea1e7fe407a0da9fa2b0e1f7e6
melikaayoughi <melikaayoughi@gmail.com>
2019-04-13 23:03:49 +0200

    finding match is between original source and target not transformed

--- Assignment1/icp/find_match.m
+++ Assignment1/icp/find_match.m
@@ -1,19 +1,17 @@
-function [matches] = find_match(A1, A2, R, t)
-%closest point is the point with lowest sum of squared errors
-transformed_A1 = R* A1 + t;
+function [matches] = find_match(A1, A2)
 matches = zeros(6, size(A1,2));
 
-for i = 1:size(transformed_A1,2)
+for i = 1:size(A1,2)
     distance = inf;
     matched_point = zeros(3,1);
     for j = 1:size(A2,2)
-        new_distance = sum(power(transformed_A1(:,i)- A2(:,j),2));
+        new_distance = sqrt(sum(power(A1(:,i)- A2(:,j),2)));
         if (distance > new_distance)
             matched_point = A2(:,j);
             distance = new_distance;
         end
     end
-    matches(:,i) = [A1(:,i);matched_point]; %A1 or transformed_A1
+    matches(:,i) = [A1(:,i);matched_point];
 end
 
 end

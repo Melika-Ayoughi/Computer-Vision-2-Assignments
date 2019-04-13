90a72f1b6c88c053116dccdbc652d0253a2126fc
melikaayoughi <melikaayoughi@gmail.com>
2019-04-13 23:01:34 +0200

    fixing minor naming problems

--- Assignment1/icp/main.m
+++ Assignment1/icp/main.m
@@ -1,8 +1,8 @@
 
 
 % 3 * 6400 double
-load('./Data/source.mat')
-load('./Data/target.mat')
+load('../Data/source.mat')
+load('../Data/target.mat')
 
 dimensions = 3;  
 [R, t] = icp(source, target, dimensions);
--- Assignment1/icp/rms.m
+++ Assignment1/icp/rms.m
@@ -1,6 +1,5 @@
 function error = rms(A1, A2, R, t)
 
-% TO DO: implement
 transformed_A1 = R* A1 + t;
 error = sqrt(sum(power(transformed_A1 - A2,2))/size(A1,2));
 

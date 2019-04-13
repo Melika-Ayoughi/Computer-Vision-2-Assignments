5d13d18e65c17b29e42761f4379c63ea30d00606
melikaayoughi <melikaayoughi@gmail.com>
2019-04-13 23:04:45 +0200

    fixed the svd part of the algorithm

--- Assignment1/icp/icp_iteration.m
+++ Assignment1/icp/icp_iteration.m
@@ -1,17 +1,17 @@
-function [R, t] = icp_iteration(A_1, A_2, R, t)
-
-% TO DO: implement
+function [R, t] = icp_iteration(A_1, A_2)
 
 centroid_1 = mean(A_1,2);
 centroid_2 = mean(A_2,2);
 
-centered_vectors_1 = A_1 - t;
-centered_vectors_2 = A_2 - t;
+centered_vectors_1 = A_1 - centroid_1;
+centered_vectors_2 = A_2 - centroid_2;
 
-covariance = cov(centered_vectors_1, centered_vectors_2);
+% covariance = cov(centered_vectors_1, centered_vectors_2);
+S = centered_vectors_1 * centered_vectors_2';
+[U,~,V] = svd(S);
+middle = eye(size(V,2));
+middle(end) = det(V*U');
 
+R = V * middle * U';
 t = centroid_2 - R * centroid_1;
-
-R = svd(covariance);
-
 end

bfd5f2f375ba234961aff6f66a3c62b7fb726650
EliasKassapis <Kassapiselias@hotmail.co.uk>
2019-04-23 13:36:29 +0200

    merging works1

--- Assignment1/main_merging_scenes.m
+++ Assignment1/main_merging_scenes.m
@@ -7,7 +7,6 @@ idx = 1;
 frame1 = get_specific_pcd_data(idx);
 frame2 = get_specific_pcd_data(idx+1);
 
-
 f1 = frame1.pcd;
 f2 = frame2.pcd;
 
@@ -16,52 +15,77 @@ f2_normals = frame2.normals;
 
 
 %Parameters
-method = "all_points";
+method = "uniform_subsamp";
 epsilon = 0.0055;
 sample_percentage = 0.1;
+s_rate = 1;
 
-[m1,m1_normals] = merge_scene(f1,f2, epsilon, method, sample_percentage, f1_normals, f2_normals);
+[m1,m1_normals] = merge_scene(f1,f2, epsilon, method, sample_percentage, f1_normals, f2_normals, s_rate, idx);
 
 
 %Plot merged scene
+figure(1)
+
 X = m1(1,:);
 Y = m1(2,:);
 Z = m1(3,:);
 
 axis equal
-scatter3(X,Y,Z, 1, color, 'filled');
+scatter3(X,Y,Z, 1, 'blue', 'filled');
 axis equal
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Q3.1 (b)
+
+%Get first frame point cliud and normals
+current_f = f1;
+current_f_normals = f1_normals;
 
 %Set sampling rate
 s_rate = 2;
 
-%get first frame index
-f = idx;
-
 %total number of frames
-N = 100;
+N = 10;
 
 %make frame dictionary
 l_struct = struct();
 
-while (f + s_rate) <  N:
+while idx <  N
+    
+    idx = idx +s_rate;
+    
+    %Get next frame's point cloud and normals
+    next_f = get_specific_pcd_data(idx);
+    n_f = next_f.pcd;
+    n_f_normals = next_f.normals;
+
+    [m,m_normals] = merge_scene(current_f,n_f, epsilon, method, sample_percentage, current_f_normals, n_f_normals, s_rate, idx);
+    
+%     str1 = 'm' + string(idx - s_rate);
+%     str2 = 'm' + string(idx - s_rate) + '_normals';
+%     
+%     %Add merged pc to struct
+%     l_struct.(str1) = m;
+%     l_struct.(str2) = m_normals;
+
+    current_f = m;
+    current_f_normals = m_normals;
     
     
-    
-    [m,m_normals] = merge_scene(f1,f2, epsilon, method, sample_percentage, f1_normals, f2_normals);
-    
-    str1 = 'm' + f;
-    str2 = 'm' + f + '_normals';
-    
-    %Add merged pc to struct
-    l_struct.(str1) = m;
-    l_struct.(str2) = m_normals;
-    
-    f+s_rate;
 end
-    
+
+
+%Plot merged scene
+
+figure(2)
+
+X = current_f(1,:);
+Y = current_f(2,:);
+Z = current_f(3,:);
+
+axis equal
+scatter3(X,Y,Z, 1, 'blue', 'filled');
+axis equal   
     
     
     
--- Assignment1/merge scenes/merge_scene.m
+++ Assignment1/merge scenes/merge_scene.m
@@ -1,4 +1,4 @@
-function[f1f2, f1f2_normals] = merge_scene(f1, f2, eps, method, sample_percentage, f1_normals, f2_normals)
+function[f1f2, f1f2_normals] = merge_scene(f1, f2, eps, method, sample_percentage, f1_normals, f2_normals, step, idx)
 
 % merge_scene(frames, frames_normals, rate, iterative_merge, reference, sampling, selection, matching, rejection, weighting, plot, name)
 
@@ -9,21 +9,27 @@ function[f1f2, f1f2_normals] = merge_scene(f1, f2, eps, method, sample_percentag
 %Using the estimated camera poses, merge the point-clouds of all the scenes into one point-cloud
 %Visualize the result
 
-%Get transformation between f1 and f2
-[R, t, ~, ~] = icp(f1,f2, eps, method, sample_percentage, f1_normals, f2_normals);
+if step == 1
+    %Get transformation between f1 and f2
+    [R, t, ~, ~] = icp(f1,f2, eps, method, sample_percentage, f1_normals, f2_normals);
 
-%Translate f1 and merge with f2 along the column axis
-f1f2 = [R*f1 + t, f2];
-f1f2_normals = [R*f1_normals, f2_normals];
+    %Translate f1 and merge with f2 along the column axis
+    f1f2 = [R*f1 + t, f2];
+    f1f2_normals = [R*f1_normals, f2_normals];
+    
+else
+    
+    %Get frame at previous step
+    previous_f = get_specific_pcd_data(idx - step);
+    p_f = previous_f.pcd;
+    p_f_normals = previous_f.normals;
+    
+    %Get transformation between f2 and previous step
+    [R, t, ~, ~] = icp(p_f,f2, eps, method, sample_percentage, p_f_normals, f2_normals);
 
-% %Plot merged scene
-% X = f1f2(1,:);
-% Y = f1f2(2,:);
-% Z = f1f2(3,:);
-% 
-% axis equal
-% scatter3(X,Y,Z, 1, color, 'filled');
-% axis equal
+    %Translate f1 and merge with f2 along the column axis
+    f1f2 = [R*f1 + t, f2];
+    f1f2_normals = [R*f1_normals, f2_normals];
 
 
-
+end

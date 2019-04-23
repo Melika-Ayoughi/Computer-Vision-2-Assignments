cbd798354f030d41c8b3e7454db6b1a8b3e60374
Stijn Verdenius <stijnverdenius@pop-os.localdomain>
2019-04-23 14:40:41 +0200

    results different methods

--- Assignment1/icp/icp.m
+++ Assignment1/icp/icp.m
@@ -1,9 +1,5 @@
 function [R_final, t_final, error, iterations] = icp(A1, A2, epsilon, method, sample_percentage, source_normals, target_normals)
-% 1. Initialize R = I (identity matrix), t = 0. -> whatsize?
-% 2. Find the closest points for each point in the base point set (A1) from the target point set (A2) using
-% brute-force approach.
-% 3. Refine R and t using Singular Value Decomposition
-% 4. Go to step 2 unless RMS is unchanged.
+
  
 disp("Start looking for R and t");
 

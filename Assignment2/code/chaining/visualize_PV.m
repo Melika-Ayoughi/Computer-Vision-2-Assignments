248414d73eb895c41e2eb5b325763b3d53d06535
EliasKassapis <Kassapiselias@hotmail.co.uk>
2019-05-03 23:15:34 +0200

    visualization for PV made binary

--- Assignment2/code/chaining/visualize_PV.m
+++ Assignment2/code/chaining/visualize_PV.m
@@ -1,5 +1,11 @@
 function [] = visualize_PV(PV) 
 
+
 binary_PV = PV > 0;
+
+%get indexes discarding the y's (not needed since binary
+idx = 1 + linspace(0,size(PV,1)-2,(size(PV,1)-2)/2+1);
+
+binary_PV = binary_PV(idx,:);
 
 imshow(binary_PV)

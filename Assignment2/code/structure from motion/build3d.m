bc72a184cede6048238cfabe2800f993f9d4dd95
melikaayoughi <melikaayoughi@gmail.com>
2019-05-07 23:14:24 +0200

    structure from motion working for test data but not really for our data, need cleaning

--- Assignment2/code/structure from motion/build3d.m
+++ Assignment2/code/structure from motion/build3d.m
@@ -1,4 +1,4 @@
-function [] = build3d(PV, seq, mode)	 % DOCSTRING_GENERATED
+function [] = build3d(PV, seq, mode, eliminate_affine)	 % DOCSTRING_GENERATED
  % BUILD3D		 
  % structure from motion
  
@@ -13,7 +13,6 @@ old_points_idx = false(1, size(PV,2));
 
 step = 2 * seq;
 for image= 1 : step : size(PV,1)-step+1
-    new_points_idx = ~ old_points_idx;
     
     dense_points_idx = all(PV(image:image+step-1, :));
     PV_dense = PV(image:image+step-1, dense_points_idx);
@@ -26,21 +25,25 @@ for image= 1 : step : size(PV,1)-step+1
     if image==1
         model(:, dense_points_idx) = s;
     else
-        intersect_idx =  old_points_idx & new_points_idx;
-        [~,~,transform] = procrustes(model(:, intersect_idx), points(:, intersect_idx));
+        intersect_idx =  old_points_idx & dense_points_idx;
+        [~,Z,transform] = procrustes(model(:, intersect_idx)', points(:, intersect_idx)');
+        
+        %update old points in model with newer iterations
+        model(:, intersect_idx) = Z';
         
         % for all points that are new 
         % add those points to the model
-        model(:, dense_points_idx & ~intersect_idx) = transform.b .* points(:, dense_points_idx & ~intersect_idx) .* transform.T + transform.c;
-        old_points_idx = old_points_idx | dense_points_idx;
+        if ~isempty(points(:, dense_points_idx & ~intersect_idx))  
+            model(:, dense_points_idx & ~intersect_idx) = (transform.b * points(:, dense_points_idx & ~intersect_idx)' * transform.T + transform.c(1,:))';
+        end
     end
+    old_points_idx = old_points_idx | dense_points_idx;
 end
 
 % visualize cloud points
 figure(5);
 scatter3(model(1,:), model(2,:), model(3,:)) %maybe some prettier function
     
-% Eliminate affine ambiguity
 
 end
 
